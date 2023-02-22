e2aaf792a464daec0cfa50ae646b106d2237d20e
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2023-02-22 18:43:25 +0545

    swipe animation done

--- client/src/components/Swipe.js
+++ client/src/components/Swipe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../assets/css/swipe.css';
+import { useEffect } from 'react'
   
 // Card component with destructured props
 //things we need
@@ -10,38 +11,53 @@ import '../assets/css/swipe.css';
 
 //reference origin ( this will be respect to the frame )
 //this is to calculate the angle of rotation
+const velocity = 10
+const maxDisplacement = 500
 
+function calculateVelocity( object ) {
+    distance(object.currentX , object.currentY)
+}
 
+function distance( x, y ){
+    return Math.sqrt( x * x + y * y )
+}
 const Swipe = () => {
     const initialValue = {
         currentX: 0,        //with refernce to the center
         currentY: 0,        //with the refernce to the center
         currentAngle: 0,    //with refernce to the initial angle
         reference: {
-            x: 0,
-            y: 0,
-            angle: 0
+            x: 0,           //with reference to center
+            y: 0,           //with refernce to center
         },
         center: {
             xo: 0,
             yo: 0
-        }
+        },
+        isDrag: false,
+        height: 0,
+        width: 0,
     }
     const [ cordinate, setCordinate ] = React.useState( initialValue )
     
     const style = { height:  '700px',
                     width: '400px',
                     background: 'grey',
-                    transformOrigin: `${ cordinate.currentAngle < 0 ? '0' : '100%'} 100%`,
-                    transform: `rotate(${-cordinate.currentAngle}deg)`,
+                    transformOrigin: `50% 100%`,
+                    transform: `rotate(${cordinate.currentAngle}deg)`,
                     position: 'relative',
                     left: cordinate.currentX - cordinate.reference.x,
-                    bottom: cordinate.currentY - cordinate.reference.y
+                    bottom: cordinate.currentY - cordinate.reference.y,
                 }
     
     const handleDragStart = (e) => {
-        let x = e.clientX - e.target.offsetLeft 
-        let y = -e.clientY + e.target.offsetHeight + e.target.offsetTop
+        e.dataTransfer.setDragImage( new Image() , e.clienttX, e.clientY)
+        let center =  {
+            xo: e.target.offsetLeft + e.target.offsetWidth / 2.0 ,
+            yo: e.target.offsetTop + e.target.offsetHeight
+        }
+        let x = e.clientX - center.xo 
+        let y = -e.clientY + center.yo
         let evaluatedState = {
             currentX: x,
             currentY: y,
@@ -49,44 +65,50 @@ const Swipe = () => {
             reference: {
                 x: x,
                 y: y,
-                angle: Math.abs( Math.atan( y / x ) * 180 / 3.14)
             },
-            center: {
-                xo: e.target.offsetLeft,
-                yo: e.target.offsetTop + e.target.offsetHeight
-            }
+            center: center,
+            isDrag: true, 
+            isSwiped: false,
+            height: e.target.offsetHeight,
+            width: e.target.offsetHeight,
         }
         setCordinate(evaluatedState)
+    
     }
     const handleDrag = (e) => {
-        e.preventDefault()
-        const x = e.clientX - cordinate.center.xo
-        const y = -e.clientY + cordinate.center.yo
-        let angle = (Math.atan(y/x)) * 180/3.14
-        if( angle < 0 )
-            angle = 180 + angle
-        angle = angle  - cordinate.reference.angle;
-        if( Math.abs( angle ) > 30 ){
-            if( angle > 0 ){
-                angle = 30
+        if( cordinate.isDrag ){
+            e.preventDefault()
+            const x = e.clientX - cordinate.center.xo
+            const y = -e.clientY + cordinate.center.yo
+            const distanceInX = x - cordinate.reference.x
+            const base = cordinate.height / 2
+            let angle = (Math.atan( distanceInX / base )) * 180/3.14
+            console.log( angle )
+            if( Math.abs( angle ) > 30 ){
+                if( angle > 0 ){
+                    angle = 30
+                }
+                else{
+                    angle = -30
             }
-            else{
-                angle = -30
-           }
+            }
+            
+            const evaluated = {
+                currentX: x,
+                currentY: y,
+                currentAngle: angle,
+            }
+            setCordinate((prev) => {
+                return ({ ...prev, ...evaluated})
+            })
+            const offsetX = Number(window.getComputedStyle( e.target ).getPropertyValue( 'left' ).slice( 0, -2))
+            const offsetY = Number(window.getComputedStyle( e.target ).getPropertyValue( 'bottom' ).slice( 0, -2))
+            if( distance(offsetX, offsetY )  > maxDisplacement ) {
+                setCordinate( initialValue )
+            }
         }
-
-        const evaluated = {
-            currentX: x,
-            currentY: y,
-            currentAngle: angle,
-        }
-        setCordinate((prev) => {
-            return ({ ...prev, ...evaluated})
-        })
-        console.log( angle,  cordinate.reference.angle)
     }
     const handleDragEnd = (e) => {
-        e.preventDefault()
         setCordinate( initialValue )
     }
     return<>
@@ -94,7 +116,7 @@ const Swipe = () => {
             <div className = 'hello' draggable style = { style }  onDragStart = { handleDragStart } onDrag = { handleDrag }  onDragEnd  = { handleDragEnd }>
 
             </div>
-
+            <div style = {{ background: 'blue', height: '20px', width: '30px' }}></div>
         </div>
     </>
 };
